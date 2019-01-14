b837e4e69a7c561ce1da2518521f3bda6f759bf7
kliegeois <kimliegeois@ymail.com>
2019-01-14 13:29:49 -0700

    Corrections of two mistakes

--- packages/stokhos/src/sacado/kokkos/vector/tpetra/Stokhos_my_gemv_MP_Vector_def.hpp
+++ packages/stokhos/src/sacado/kokkos/vector/tpetra/Stokhos_my_gemv_MP_Vector_def.hpp
@@ -13,9 +13,9 @@ void my_gemv (const char trans[],
 {
   // y := alpha*A*x + beta*y,
 
-  static_assert (Kokkos::View<DA,PA...>::rank == 2, "GEMM: A must have rank 2 (be a matrix).");
-  static_assert (Kokkos::View<DX,PX...>::rank == 1, "GEMM: x must have rank 1 (be a vector).");
-  static_assert (Kokkos::View<DY,PY...>::rank == 1, "GEMM: y must have rank 1 (be a vector).");
+  static_assert (VA::rank == 2, "GEMM: A must have rank 2 (be a matrix).");
+  static_assert (VX::rank == 1, "GEMM: x must have rank 1 (be a vector).");
+  static_assert (VY::rank == 1, "GEMM: y must have rank 1 (be a vector).");
   
   // Get the dimensions
   const size_t m = y.dimension_0 ();
