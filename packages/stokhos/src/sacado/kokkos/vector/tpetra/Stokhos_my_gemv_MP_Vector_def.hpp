059b56698354bad272091a16f3ae64a3b3bf4c0f
Kim Liégeois <liegeois@gaston.ltas.ulg.ac.be>
2019-01-15 22:04:23 +0100

    Correction of some errors in the gemv for MP Vector

--- packages/stokhos/src/sacado/kokkos/vector/tpetra/Stokhos_my_gemv_MP_Vector_def.hpp
+++ packages/stokhos/src/sacado/kokkos/vector/tpetra/Stokhos_my_gemv_MP_Vector_def.hpp
@@ -105,7 +105,7 @@ void inner_product_kernel(T * A, T alpha, T * b, T * c, int i_max);
 #undef size
 #undef n_vectors
 
-template<class Storage,
+template<class Scalar,
          class VA,
          class VX,
          class VY>
@@ -120,19 +120,16 @@ void my_update (
   const size_t m = y.dimension_0 ();
   const size_t n = x.dimension_0 ();
   
-  typedef Sacado::MP::Vector<Storage> Scalar;
-  
   const size_t m_c = Sacado_MP_Vector_GEMV_Tile_Size(sizeof(Scalar));
   const size_t n_vectors = Sacado_MP_Vector_GEMV_Number_Vectors(sizeof(Scalar));
 
   const int n_tiles = ceil(((double) m)/m_c);
 
-  //Kokkos::parallel_for (n_tiles, KOKKOS_LAMBDA (const int i_tile)
-  for (size_t i_tile = 0; i_tile<n_tiles; ++i_tile)
+  Kokkos::parallel_for (n_tiles, KOKKOS_LAMBDA (const int i_tile)
   {
-    size_t i_min = m_c*i_tile;
+    int i_min = m_c*i_tile;
     bool last_tile = (i_tile==(n_tiles-1));
-    size_t i_max = (last_tile) ? m : (i_min+m_c);
+    int i_max = (last_tile) ? m : (i_min+m_c);
 
     #pragma unroll
     for ( size_t i=i_min; i<i_max; ++i )
@@ -148,11 +145,10 @@ void my_update (
     
     for ( size_t j=0; j<n; ++j )
       update_kernel<Scalar>(&A(i_min,j),alpha,&x(j),&y(i_min),i_max-i_min);
-  }
-  //);
+  });
 }
 
-template<class Storage,
+template<class Scalar,
          class VA,
          class VX,
          class VY>
@@ -166,9 +162,46 @@ void my_inner_product (
   // Get the dimensions
   const size_t m = y.dimension_0 ();
   const size_t n = x.dimension_0 ();
+
+  const int pool_size = Kokkos::DefaultExecutionSpace::thread_pool_size();
+
+  const int team_size = 4;
+
+  const size_t m_c = Sacado_MP_Vector_GEMV_Tile_Size(sizeof(Scalar));
+  const int n_per_tile2 = m_c*team_size;
+
+  const int n_i2 = ceil(((double) n)/n_per_tile2);
+  using Kokkos::TeamThreadRange;
+
+  Kokkos::TeamPolicy<> policy = Kokkos::TeamPolicy<>( n_i2, team_size);
+  typedef Kokkos::TeamPolicy<>::member_type member_type;
+  
+  Kokkos::parallel_for (m, KOKKOS_LAMBDA (const int i)
+  {
+      y(i) *= beta;
+  });
+
+  Kokkos::parallel_for (policy, KOKKOS_LAMBDA (member_type team_member)
+  {
+    const int j = team_member.league_rank();
+    const int j_min = n_per_tile2*j;
+    const int j_max = (j_min+n_per_tile2 > n) ? n : j_min+n_per_tile2;
+    const int i_min = (j<m) ? j:(j%m);
+
+    for ( int i=i_min; i<m; ++i ){
+      Scalar tmp = 0.;
+      Kokkos::parallel_reduce (TeamThreadRange (team_member, j_max-j_min),[=] (int jj, Scalar& tmp_sum){
+        tmp_sum += A(jj+j_min,i) * x(jj+j_min);
+      },tmp);
+      tmp *= alpha;
+      if ( team_member.team_rank () == 0) {
+        Kokkos::atomic_add(&y(i),tmp);
+      }
+    }
+  });
 }
 
-template<class Storage,
+template<class Scalar,
          class VA,
          class VX,
          class VY>
@@ -187,10 +220,10 @@ void my_gemv (const char trans[],
   
   if (trans[0]=='n'||trans[0]=='N')
   {
-    my_update<Storage,VA,VX,VY>(alpha,A,x,beta,y);
+    my_update<Scalar,VA,VX,VY>(alpha,A,x,beta,y);
   }
   else
   {
-    my_inner_product<Storage,VA,VX,VY>(alpha,A,x,beta,y);
+    my_inner_product<Scalar,VA,VX,VY>(alpha,A,x,beta,y);
   }
 }
